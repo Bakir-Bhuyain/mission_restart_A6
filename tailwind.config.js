cfc84ce11473b753df33f82b150148ef2175d33b
Bakir-Bhuyain <hemalbakir@gmail.com>
2026-02-15 23:34:29 +0600

    nav completed with daisyui and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js (v3.x example)
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Paths to your project files
+  ],
+  theme: {
+    extend: {}, // For extending the default theme
+  },
+  plugins: [], // For adding plugins
+};
